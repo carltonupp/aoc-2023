dd93149631687418ac17d29d88a4f2e41a8ac168
carltonupperdine <carlton.upperdine@gmail.com>
2023-12-02 15:21:11 +0000

    inline total

--- solutions/day2/part2.fsx
+++ solutions/day2/part2.fsx
@@ -2,7 +2,7 @@ open System.Text.RegularExpressions
 
 let inputs = List.ofSeq(System.IO.File.ReadLines "./solutions/day2/inputs.txt")
 
-let totals= [ for input in inputs do
+let total= [ for input in inputs do
                             let matches = Regex.Matches(input, @"\d{1,} \w{1,}")
                             let pairs = [ for m in matches do m.Value.Split(" ") |> fun pair -> (pair[0] |> int, pair[1]) ]
 
@@ -13,6 +13,4 @@ let totals= [ for input in inputs do
                             for (number, color) in highestInEachGroup do
                                 total <- total * number
 
-                            total ]
-
-totals |> List.sum
+                            total ] |> List.sum
