4ea5b470838f572f3480e6a1a0ee56b357b95edb
carltonupperdine <carlton.upperdine@gmail.com>
2023-12-02 16:34:38 +0000

    slight refactor

--- solutions/day2/part2.fsx
+++ solutions/day2/part2.fsx
@@ -2,15 +2,14 @@ open System.Text.RegularExpressions
 
 let inputs = List.ofSeq(System.IO.File.ReadLines "./solutions/day2/inputs.txt")
 
+let getTotal lines =
+    lines
+    |> List.fold (fun total (n, _) -> total * n) 1
+    
+
 let total= [ for input in inputs do
-                            let matches = Regex.Matches(input, @"\d{1,} \w{1,}")
-                            let pairs = [ for m in matches do m.Value.Split(" ") |> fun pair -> (pair[0] |> int, pair[1]) ]
-
-                            let grouped = pairs |> List.groupBy (fun (number, color) -> color )
-                            let highestInEachGroup = grouped 
-                                                                        |> List.map (fun (group, results) -> List.maxBy (fun (number, color) -> number) results)
-                            let mutable total = 1
-                            for (number, color) in highestInEachGroup do
-                                total <- total * number
-
-                            total ] |> List.sum
+                        [ for m in Regex.Matches(input, @"\d{1,} \w{1,}") do 
+                            m.Value.Split(" ") |> fun pair -> (pair[0] |> int, pair[1]) ]
+                            |> List.groupBy (fun (number, color) -> color )
+                            |> List.map (fun (group, results) -> List.maxBy (fun (number, color) -> number) results)
+                            |> getTotal ] |> List.sum
